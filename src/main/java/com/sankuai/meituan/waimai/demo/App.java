7279f847c362bca1ac5ff1cc2d655809ba0b991b
wangchaochao02 <wangchaochao02@meituan.com>
2020-03-15 11:52:02 +0800

    工程结构初始化

---
+++ src/main/java/com/sankuai/meituan/waimai/demo/App.java
@@ -0,0 +1,13 @@
+package com.sankuai.meituan.waimai.demo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
